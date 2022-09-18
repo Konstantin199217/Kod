cfe42daf2b79d133977def33066e4353a4642ccd
Konstantin <kot199217@mail.ru>
2022-09-18 23:17:59 +0900

    nosing to comit

---
+++ Example011/Program.cs
@@ -0,0 +1,48 @@
+﻿void FillArray(int[] collection)
+{
+    int Length = collection.Length;
+    int index = 0;
+    while (index < Length)
+    {
+        ///                 новое случайное число из диапазона от 1 до 10
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = 0;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];//массив с 10 элементами
+
+FillArray(array);//заполнил массив
+PrintArray(array);// распечатал массив
+
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
